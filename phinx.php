fd2c150146c46276c3a2ad98d0f6a0cad7c33486
Alex White <hi@thatalexguy.dev>
2026-03-10 21:28:28 -0400

    phinx update

--- phinx.php
+++ phinx.php
@@ -1,28 +1,28 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . "/vendor/autoload.php";
 
 $dotenv = Dotenv\Dotenv::createMutable(__DIR__);
 $dotenv->safeLoad();
 
 return [
-    'paths' => [
-        'migrations' => '%%PHINX_CONFIG_DIR%%/database/migrations',
-        'seeds'      => '%%PHINX_CONFIG_DIR%%/database/seeds',
+    "paths" => [
+        "migrations" => "%%PHINX_CONFIG_DIR%%/database/migrations",
+        "seeds" => "%%PHINX_CONFIG_DIR%%/database/seeds",
     ],
-    'environments' => [
-        'default_migration_table' => 'phinxlog',
-        'default_environment'     => 'development',
-        'development' => [
-            'adapter' => 'mysql',
-            'host'    => getenv('DB_HOST')     ?: '127.0.0.1',
-            'name'    => getenv('DB_NAME')     ?: 'kai',
-            'user'    => getenv('DB_USER')     ?: 'root',
-            'pass'    => getenv('DB_PASSWORD') ?: '',
-            'port'    => getenv('DB_PORT')     ?: '3306',
-            'charset' => 'utf8mb4',
+    "environments" => [
+        "default_migration_table" => "phinxlog",
+        "default_environment" => "development",
+        "development" => [
+            "adapter" => "mysql",
+            "host" => $_ENV["DB_HOST"] ?? getenv("DB_HOST") ?: "127.0.0.1",
+            "name" => $_ENV["DB_NAME"] ?? getenv("DB_NAME") ?: "kai",
+            "user" => $_ENV["DB_USER"] ?? getenv("DB_USER") ?: "root",
+            "pass" => $_ENV["DB_PASSWORD"] ?? getenv("DB_PASSWORD") ?: "",
+            "port" => $_ENV["DB_PORT"] ?? getenv("DB_PORT") ?: "3306",
+            "charset" => "utf8mb4",
         ],
     ],
-    'version_order' => 'creation',
+    "version_order" => "creation",
 ];
