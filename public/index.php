a75678001b4749680fa90718704d1ababecdd53e
Alex White <hi@thatalexguy.dev>
2026-03-10 21:13:08 -0400

    Update

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ use Slim\Factory\AppFactory;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv = Dotenv\Dotenv::createMutable(__DIR__ . '/../');
 $dotenv->safeLoad();
 
 $containerBuilder = new ContainerBuilder();
