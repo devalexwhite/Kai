cad2dd16e8f2086b003edd8e9505c992f3394971
Alex White <hi@thatalexguy.dev>
2026-03-10 21:21:31 -0400

    Update

--- config/settings.php
+++ config/settings.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 return [
     'displayErrors' => true,
     'db' => [
-        'host'     => getenv('DB_HOST')     ?: '127.0.0.1',
-        'port'     => getenv('DB_PORT')     ?: '3306',
-        'name'     => getenv('DB_NAME')     ?: 'kai',
-        'user'     => getenv('DB_USER')     ?: 'root',
-        'password' => getenv('DB_PASSWORD') ?: '',
+        'host'     => $_ENV['DB_HOST']     ?? getenv('DB_HOST')     ?: '127.0.0.1',
+        'port'     => $_ENV['DB_PORT']     ?? getenv('DB_PORT')     ?: '3306',
+        'name'     => $_ENV['DB_NAME']     ?? getenv('DB_NAME')     ?: 'kai',
+        'user'     => $_ENV['DB_USER']     ?? getenv('DB_USER')     ?: 'root',
+        'password' => $_ENV['DB_PASSWORD'] ?? getenv('DB_PASSWORD') ?: '',
     ],
     'twig' => [
         'templatePath' => __DIR__ . '/../templates',
